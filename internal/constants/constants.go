892d4250a43c18a4d2589c48c0a9636cf537689c
Арсений <arsenypadaltsyn@yandex.ru>
2025-10-10 14:52:21 +0300

    rate info text

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -5,4 +5,5 @@ const (
 	AdminAccount     = "https://t.me/s3mmm_7"
 	StepsToMakeOrder = "Для заказа отправьте @s3mmm_7 в лс:\n\n1️⃣Ссылку на товар\n2️⃣Укажите размер товара\n3️⃣При необходимости цвет товара"
 	PoizonGuide      = "https://telegra.ph/Kak-poschitat-konechnuyu-cenu-za-tovar-i-zakazat-02-28"
+	RateInfo         = "Курс на сегодня: 12.2₽ за 1¥"
 )
