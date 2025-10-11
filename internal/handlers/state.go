8497d65b2771e8d7476d97e64a205ba30fdbb511
Арсений <arsenypadaltsyn@yandex.ru>
2025-10-11 16:20:12 +0300

    tracking last menu message

---
+++ internal/handlers/state.go
@@ -0,0 +1,27 @@
+package handlers
+
+import "sync"
+
+var (
+	muLast      sync.RWMutex
+	lastMenuMsg = map[int64]int{}
+)
+
+func SetLastMenuMessage(chatID int64, messageID int) {
+	muLast.Lock()
+	defer muLast.Unlock()
+	lastMenuMsg[chatID] = messageID
+}
+
+func GetLastMenuMessage(chatID int64) (int, bool) {
+	muLast.RLock()
+	defer muLast.RUnlock()
+	id, ok := lastMenuMsg[chatID]
+	return id, ok
+}
+
+func ClearLastMenuMessage(chatID int64) {
+	muLast.Lock()
+	defer muLast.Unlock()
+	delete(lastMenuMsg, chatID)
+}
