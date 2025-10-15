8a201722975576f17ba9245d9ec2e955c8ff054f
Арсений <arsenypadaltsyn@yandex.ru>
2025-10-15 13:31:34 +0300

    set shipping cost config for calculator

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	config := configs.LoadConfig(".env")
-	calculator.SetConfig(&config.CommissionConfig)
+	calculator.SetConfig(&config.CommissionConfig, &config.ShippingCostConfig)
 	rate.SetConfig(&config.DefaultRatesConfig)
 
 	tgBot, err := bot.New(config.TelegramBotToken)
