a97733f1f7a22ba8711f34f34bdc6bbb32401724
Арсений <arsenypadaltsyn@yandex.ru>
2025-10-15 13:25:24 +0300

    config for shipping cost

--- configs/config.go
+++ configs/config.go
@@ -13,6 +13,7 @@ type Config struct {
 	TelegramBotToken   string
 	CommissionConfig   СommissionConfig
 	DefaultRatesConfig DefaultRatesConfig
+	ShippingCostConfig ShippingCostConfig
 }
 
 type СommissionConfig struct {
@@ -21,8 +22,33 @@ type СommissionConfig struct {
 }
 
 type DefaultRatesConfig struct {
-	DefaultRateForCNY float64
-	DefaultRateForEUR float64
+	DefaultRateCNY_RUB float64
+	DefaultRateEUR_RUB float64
+	DefaultRateRUB_EUR float64
+}
+
+type ShippingCostConfig struct {
+	TShirtShippingCost        int
+	HoodieShippingCost        int
+	JacketShippingCost        int
+	CoatShippingCost          int
+	PantsShippingCost         int
+	ShortsShippingCost        int
+	SocksShippingCost         int
+	HatsShippingCost          int
+	SneakersShippingCost      int
+	OtherSneakersShippingCost int
+	BootsShippingCost         int
+	HeelsShippingCost         int
+	SlippersShippingCost      int
+	SandalsShippingCost       int
+	GlassesShippingCost       int
+	GlovesShippingCost        int
+	JewerlyShippingCost       int
+	WatchesShippingCost       int
+	BeltShippingCost          int
+	HeaddressShippingCost     int
+	BagShippingCost           int
 }
 
 func LoadConfig(envPath string) *Config {
@@ -32,14 +58,36 @@ func LoadConfig(envPath string) *Config {
 		}
 	}
 
-	shoesCom, err1 := strconv.Atoi(os.Getenv("COMMISSION_FOR_SHOES"))
-	otherCom, err2 := strconv.Atoi(os.Getenv("COMMISSION_FOR_OTHER"))
-	defaultRateForCNY, err3 := strconv.ParseFloat(os.Getenv("CNY_DEFAULT_RATE"), 64)
-	defaultRateForEUR, err4 := strconv.ParseFloat(os.Getenv("EUR_DEFAULT_RATE"), 64)
+	shoesCom := getEnvInt("COMMISSION_FOR_SHOES")
+	otherCom := getEnvInt("COMMISSION_FOR_OTHER")
 
-	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
-		logger.Log.Fatal(constants.EnvVariablesErrorOutput)
-	}
+	defaultRateCNY_RUB := getEnvFloat("CNY_RUB_DEFAULT_RATE")
+	defaultRateEUR_RUB := getEnvFloat("EUR_RUB_DEFAULT_RATE")
+	defaultRateRUB_EUR := getEnvFloat("RUB_EUR_DEFAULT_RATE")
+
+	var shippingCostConfig ShippingCostConfig
+
+	shippingCostConfig.TShirtShippingCost = getEnvInt("TSHIRT_SHIPPING_COST")
+	shippingCostConfig.HoodieShippingCost = getEnvInt("HOODIE_SHIPPING_COST")
+	shippingCostConfig.JacketShippingCost = getEnvInt("JACKET_SHIPPING_COST")
+	shippingCostConfig.CoatShippingCost = getEnvInt("COAT_SHIPPING_COST")
+	shippingCostConfig.PantsShippingCost = getEnvInt("PANTS_SHIPPING_COST")
+	shippingCostConfig.ShortsShippingCost = getEnvInt("SHORTS_SHIPPING_COST")
+	shippingCostConfig.SocksShippingCost = getEnvInt("SOCKS_SHIPPING_COST")
+	shippingCostConfig.HatsShippingCost = getEnvInt("HATS_SHIPPING_COST")
+	shippingCostConfig.SneakersShippingCost = getEnvInt("SNEAKERS_SHIPPING_COST")
+	shippingCostConfig.OtherSneakersShippingCost = getEnvInt("OTHER_SNEAKERS_SHIPPING_COST")
+	shippingCostConfig.BootsShippingCost = getEnvInt("BOOTS_SHIPPING_COST")
+	shippingCostConfig.HeelsShippingCost = getEnvInt("HEELS_SHIPPING_COST")
+	shippingCostConfig.SlippersShippingCost = getEnvInt("SLIPPERS_SHIPPING_COST")
+	shippingCostConfig.SandalsShippingCost = getEnvInt("SANDALS_SHIPPING_COST")
+	shippingCostConfig.GlassesShippingCost = getEnvInt("GLASSES_SHIPPING_COST")
+	shippingCostConfig.GlovesShippingCost = getEnvInt("GLOVES_SHIPPING_COST")
+	shippingCostConfig.JewerlyShippingCost = getEnvInt("JEWERLY_SHIPPING_COST")
+	shippingCostConfig.WatchesShippingCost = getEnvInt("WATCHES_SHIPPING_COST")
+	shippingCostConfig.BeltShippingCost = getEnvInt("BELT_SHIPPING_COST")
+	shippingCostConfig.HeaddressShippingCost = getEnvInt("HEADDRESS_SHIPPING_COST")
+	shippingCostConfig.BagShippingCost = getEnvInt("BAG_SHIPPING_COST")
 
 	cfg := &Config{
 		TelegramBotToken: os.Getenv("TELEGRAM_BOT_TOKEN"),
@@ -48,10 +96,28 @@ func LoadConfig(envPath string) *Config {
 			CommissionForOther: otherCom,
 		},
 		DefaultRatesConfig: DefaultRatesConfig{
-			DefaultRateForCNY: defaultRateForCNY,
-			DefaultRateForEUR: defaultRateForEUR,
+			DefaultRateCNY_RUB: defaultRateCNY_RUB,
+			DefaultRateEUR_RUB: defaultRateEUR_RUB,
+			DefaultRateRUB_EUR: defaultRateRUB_EUR,
 		},
+		ShippingCostConfig: shippingCostConfig,
 	}
 
 	return cfg
+}
+
+func getEnvInt(key string) int {
+	val, err := strconv.Atoi(os.Getenv(key))
+	if err != nil {
+		logger.Log.Fatalf(constants.EnvVariablesErrorOutput, key)
+	}
+	return val
+}
+
+func getEnvFloat(key string) float64 {
+	val, err := strconv.ParseFloat(os.Getenv(key), 64)
+	if err != nil {
+		logger.Log.Fatalf(constants.EnvVariablesErrorOutput, key)
+	}
+	return val
 }
