acb1fc6a7a20d2424f432f6c811b2340484ef4de
Арсений <arsenypadaltsyn@yandex.ru>
2025-10-15 13:30:36 +0300

    computing final price including shipping cost to Russia

--- internal/calculator/calculator.go
+++ internal/calculator/calculator.go
@@ -10,44 +10,46 @@ import (
 	"github.com/SenechkaP/semstore-bot/configs"
 	"github.com/SenechkaP/semstore-bot/internal/constants"
 	"github.com/SenechkaP/semstore-bot/internal/format"
+	"github.com/SenechkaP/semstore-bot/internal/logger"
 	"github.com/SenechkaP/semstore-bot/internal/rate"
 )
 
 var items map[string]Item
 
-func SetConfig(cfg *configs.СommissionConfig) {
+func SetConfig(cfgCom *configs.СommissionConfig, cfgShip *configs.ShippingCostConfig) {
 	items = map[string]Item{
-		"sneakers":       {Name: "Кроссовки", Сommission: cfg.CommissionForShoes},
-		"other_sneakers": {Name: "Кеды", Сommission: cfg.CommissionForShoes},
-		"boots":          {Name: "Ботинки", Сommission: cfg.CommissionForShoes},
-		"heels":          {Name: "Туфли", Сommission: cfg.CommissionForShoes},
-		"slippers":       {Name: "Тапки", Сommission: cfg.CommissionForShoes},
-		"sandals":        {Name: "Сандали", Сommission: cfg.CommissionForShoes},
+		"sneakers":       {Name: "Кроссовки", Сommission: cfgCom.CommissionForShoes, ShippingCost: cfgShip.SneakersShippingCost},
+		"other_sneakers": {Name: "Кеды", Сommission: cfgCom.CommissionForShoes, ShippingCost: cfgShip.OtherSneakersShippingCost},
+		"boots":          {Name: "Ботинки", Сommission: cfgCom.CommissionForShoes, ShippingCost: cfgShip.BootsShippingCost},
+		"heels":          {Name: "Туфли", Сommission: cfgCom.CommissionForShoes, ShippingCost: cfgShip.HeelsShippingCost},
+		"slippers":       {Name: "Тапки", Сommission: cfgCom.CommissionForShoes, ShippingCost: cfgShip.SlippersShippingCost},
+		"sandals":        {Name: "Сандали", Сommission: cfgCom.CommissionForShoes, ShippingCost: cfgShip.SandalsShippingCost},
 
-		"shirts":  {Name: "Футболка/Рубашка", Сommission: cfg.CommissionForOther},
-		"hoodies": {Name: "Толстовка/Худи", Сommission: cfg.CommissionForOther},
-		"coats":   {Name: "Пуховик/Пальто", Сommission: cfg.CommissionForOther},
-		"jackets": {Name: "Жилетка/Куртка", Сommission: cfg.CommissionForOther},
-		"pants":   {Name: "Штаны", Сommission: cfg.CommissionForOther},
-		"shorts":  {Name: "Шорты", Сommission: cfg.CommissionForOther},
-		"hats":    {Name: "Шапка/Кепка", Сommission: cfg.CommissionForOther},
-		"socks":   {Name: "Носки", Сommission: cfg.CommissionForOther},
+		"shirts":  {Name: "Футболка/Рубашка", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.TShirtShippingCost},
+		"hoodies": {Name: "Толстовка/Худи", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.HoodieShippingCost},
+		"coats":   {Name: "Пуховик/Пальто", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.CoatShippingCost},
+		"jackets": {Name: "Жилетка/Куртка", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.JacketShippingCost},
+		"pants":   {Name: "Штаны", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.PantsShippingCost},
+		"shorts":  {Name: "Шорты", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.ShortsShippingCost},
+		"hats":    {Name: "Шапка/Кепка", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.HatsShippingCost},
+		"socks":   {Name: "Носки", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.SocksShippingCost},
 
-		"glasses":   {Name: "Очки", Сommission: cfg.CommissionForOther},
-		"watches":   {Name: "Часы", Сommission: cfg.CommissionForOther},
-		"jewelry":   {Name: "Украшение", Сommission: cfg.CommissionForOther},
-		"belts":     {Name: "Ремень", Сommission: cfg.CommissionForOther},
-		"gloves":    {Name: "Перчатки", Сommission: cfg.CommissionForOther},
-		"headdress": {Name: "Головной убор", Сommission: cfg.CommissionForOther},
+		"glasses":   {Name: "Очки", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.GlassesShippingCost},
+		"watches":   {Name: "Часы", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.WatchesShippingCost},
+		"jewelry":   {Name: "Украшение", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.JewerlyShippingCost},
+		"belts":     {Name: "Ремень", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.BeltShippingCost},
+		"gloves":    {Name: "Перчатки", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.GlovesShippingCost},
+		"headdress": {Name: "Головной убор", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.HeaddressShippingCost},
 
-		"bags":     {Name: "Рюкзак/Сумка", Сommission: cfg.CommissionForOther},
-		"continue": {Name: "Другое", Сommission: 0},
+		"bags":     {Name: "Рюкзак/Сумка", Сommission: cfgCom.CommissionForOther, ShippingCost: cfgShip.BagShippingCost},
+		"continue": {Name: "Другое", Сommission: 0, ShippingCost: 0},
 	}
 }
 
 type Item struct {
-	Name       string
-	Сommission int
+	Name         string
+	Сommission   int
+	ShippingCost int
 }
 
 var (
@@ -82,12 +84,31 @@ func ClearPending(chatID int64) {
 }
 
 func Compute(item Item, price int) (float64, string) {
-	rateCNY, _ := rate.GetRate("CNY")
-	total := rateCNY*float64(price) + float64(item.Сommission)
+	rateCNY_RUB, _ := rate.GetRate("CNY")
+	rateEUR_RUB, _ := rate.GetRate("EUR")
+	rateRUB_EUR, _ := rate.GetRubEur()
+
+	priceInRUB := rateCNY_RUB * float64(price)
+	priceInEUR := priceInRUB * rateRUB_EUR
+	logger.Log.Info("price in CNY = ", price)
+	logger.Log.Info("price in EUR = ", priceInEUR)
+
+	var additionalTax float64
+	var additionalText string
+
+	if priceInEUR > 200 {
+		additionalTax = (priceInEUR-200)*0.15*rateEUR_RUB + 500
+		additionalText = fmt.Sprintf(constants.OverTwoHundredEurText, additionalTax)
+	}
+
+	total := priceInRUB + float64(item.Сommission) + float64(item.ShippingCost) + additionalTax
+	totalString := format.FormatNumberWithDots(int(math.Ceil(total)))
+	totalString = totalString[:len(totalString)-2] + "90"
 	text := fmt.Sprintf(constants.PriceOutput,
 		item.Name,
 		format.FormatNumberWithDots(price),
-		format.FormatNumberWithDots(int(math.Ceil(total))),
+		totalString,
+		additionalText,
 	)
 	return total, text
 }
