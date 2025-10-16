25889f02b4a410bcd0eb9d94f4e54a89f108eda6
Арсений <arsenypadaltsyn@yandex.ru>
2025-10-16 21:04:51 +0300

    using exchange service

--- internal/rate/rate.go
+++ internal/rate/rate.go
@@ -1,63 +1,29 @@
 package rate
 
 import (
-	"encoding/json"
-	"io"
 	"math"
-	"net/http"
 
-	"github.com/SenechkaP/semstore-bot/configs"
+	"github.com/SenechkaP/semstore-bot/internal/services"
 )
 
-var defaultRates map[string]float64
+var exchangeServise *services.ExchangeService
 
-func SetConfig(cfg *configs.DefaultRatesConfig) {
-	defaultRates = map[string]float64{
-		"CNY":     cfg.DefaultRateCNY_RUB,
-		"EUR":     cfg.DefaultRateEUR_RUB,
-		"RUB_EUR": cfg.DefaultRateRUB_EUR,
-	}
-}
-
-type ExchangeRates struct {
-	Rates map[string]float64 `json:"rates"`
+func SetService(service *services.ExchangeService) {
+	exchangeServise = service
 }
 
 // Returns amount of RUB for 1 unit of currency
 func GetRate(currency string) (float64, error) {
-	allRates, err := getExchangeRate()
-	if err != nil {
-		return defaultRates[currency], err
-	}
-
-	rate := math.Round(1/allRates.Rates[currency]*100) / 100
+	curRate, err := exchangeServise.GetRate(currency)
+	rate := math.Round(1/curRate*100) / 100
 
 	if currency == "CNY" {
 		rate += 0.7
 	}
 
-	return rate, nil
+	return rate, err
 }
 
 func GetRubEur() (float64, error) {
-	allRates, err := getExchangeRate()
-	if err != nil {
-		return defaultRates["RUB_EUR"], err
-	}
-	return allRates.Rates["EUR"], nil
-}
-
-func getExchangeRate() (*ExchangeRates, error) {
-	responce, err := http.Get("https://www.cbr-xml-daily.ru/latest.js")
-	if err != nil {
-		return nil, err
-	}
-	defer responce.Body.Close()
-	body, err := io.ReadAll(responce.Body)
-	if err != nil {
-		return nil, err
-	}
-	var rates ExchangeRates
-	json.Unmarshal(body, &rates)
-	return &rates, nil
+	return exchangeServise.GetRate("EUR")
 }
